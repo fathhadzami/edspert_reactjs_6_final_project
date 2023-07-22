ccd6a2d748ebf87a3b9281c8b5d7ddbe6cf86397
fathhadzami <fath.hadzami@gmail.com>
2023-07-22 22:03:20 +0700

    add update delete course

--- src/materi-CRUD/utils/service.js
+++ src/materi-CRUD/utils/service.js
@@ -20,9 +20,35 @@ const getCourses = () => {
   return response;
 }
 
+const updateCourse = (courseId, payload) => {
+  const courses = storageManager.get();
+  const updatedCourse = courses.map((item) => {
+    if (item.id === courseId) {
+      return {
+        ...item,
+        ...payload,
+      };
+    }
+    return item;
+  });
+
+  storageManager.set(updatedCourse);
+  return {
+    data: payload,
+  }
+}
+
+const deleteCourse = (courseId) => {
+  const courses = storageManager.get();
+  const newCourses = courses.filter((item) => item.id !== courseId);
+  storageManager.set(newCourses);
+}
+
 const courseService = {
   addCourse,
   getCourses,
+  updateCourse,
+  deleteCourse,
 };
 
 export default courseService;
