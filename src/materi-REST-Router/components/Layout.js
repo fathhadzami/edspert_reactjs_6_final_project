1e92840719eba9674132b1dfdecbb57fa93bc757
fathhadzami <fath.hadzami@gmail.com>
2023-07-23 08:03:18 +0700

    setup navigate router

--- src/materi-REST-Router/components/Layout.js
+++ src/materi-REST-Router/components/Layout.js
@@ -1,10 +1,18 @@
-import { Outlet } from "react-router-dom";
-
+import { Button } from "react-bootstrap";
+import { Outlet, useNavigate } from "react-router-dom";
 
 const Layout = () => {
+  const navigate = useNavigate();
   return (
     <>
-      Ini adalah layout
+      <div>
+        Ini adalah layout
+        <div style={{ display: "flex", gap: "8px" }}>
+          <Button onClick={() => navigate("/")}>Product List</Button>{' '}
+          <Button onClick={() => navigate("/detail/1")}>Detail</Button>{' '}
+          <Button onClick={() => navigate("/checkout")}>Checkout</Button>{' '}
+        </div>
+      </div>
       <Outlet />
     </>
   );
